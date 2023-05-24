0b82d38c382d12d030c00a55b9e7cb9919f7af37
Barto <juanmartin.cerruti@gmail.com>
2023-05-24 10:18:09 -0300

    add changes

--- game/Scene1.ts
+++ game/Scene1.ts
@@ -33,12 +33,12 @@ class Scene1 extends Phaser.Scene {
     this.map = new Mapa(this);
     this.map.createMap();
     const { x, y } = this.map.startingPoint;
-    this.monchi = new Player(this, x, y, "character", 2); 
+    this.monchi = new Player(this, x, y, "character", 2);
     this.canWin = false
     this.canRot = true
     /* Camera */
     this.cameras.main.startFollow(this.monchi)
-    
+
 
     const touch = () => {
       if (this.monchi) this.monchi.idle()
@@ -47,55 +47,52 @@ class Scene1 extends Phaser.Scene {
       if (this.monchi) {
         this.monchi.setBounce(0)
         this.monchi.setGravityY(-2000)
-        this.time.delayedCall(1000, () =>
-        {
-            this.monchi?.setFlipY(true)
-            this.monchi?.body?.setOffset(70,0)
-            this.monchi?.setBounceY(0)
+        this.time.delayedCall(1000, () => {
+          this.monchi?.setFlipY(true)
+          this.monchi?.body?.setOffset(70, 0)
+          this.monchi?.setBounceY(0)
         })
       }
     }
 
     const rotateCam = () => {
       this.normalito = false
-      this.canRot = false
-      let allowed = true
-      if(allowed){
+      // let allowed = true
+      if (this.canRot) {
         let rotation = 0
-        for(let i = 0 ; i < 13 ; i++){
-          rotation = 3.1415*i/12
-          this.time.delayedCall(100, () =>
-            {
-              this.cameras.main.setRotation(rotation)
-            })
-            if(i==12){allowed = false}
+        for (let i = 0; i < 13; i++) {
+          this.time.delayedCall(100 * i, () => ((rotate) => {
+            this.cameras.main.setRotation(rotate)
+          })(3.1415 * i / 12))
+          if (i == 12) { this.canRot = false }
         }
       }
-      else{  this.cameras.main.setRotation(3.1415)
+      else {
+        console.log("se escapo")
+        // this.cameras.main.setRotation(3.1415)
 
       }
-      }
-    
+    }
+
 
     const noFloat = () => {
       this.normalito = true
       if (this.monchi) {
-        this.time.delayedCall(100, () =>
-        {
-            this.monchi?.setGravity(0)
-            this.cameras.main.setRotation(0)
-            this.monchi?.setFlipY(false)
-            this.monchi?.body?.setOffset(70,50)
-            this.monchi?.setBounceY(0)
+        this.time.delayedCall(100, () => {
+          this.monchi?.setGravity(0)
+          this.cameras.main.setRotation(0)
+          this.monchi?.setFlipY(false)
+          this.monchi?.body?.setOffset(70, 50)
+          this.monchi?.setBounceY(0)
         })
+      }
     }
-  }
     const lose = () => {
       this.scene.restart()
       this.normalito = true
     }
     const win = () => {
-      if(this.canWin){
+      if (this.canWin) {
         this.scene.restart()
       }
     }
@@ -113,15 +110,17 @@ class Scene1 extends Phaser.Scene {
         this.map.coin.clear(true)
       }
     }
-    if(this.map.portal) this.map.portal.setTint(0xff0000)
+    if (this.map.portal) this.map.portal.setTint(0xff0000)
     if (this.map.pisos) this.physics.add.collider(this.monchi, this.map.pisos, touch);
     if (this.map.pisos2) this.physics.add.collider(this.monchi, this.map.pisos2, float);
     if (this.map.pisos3) this.physics.add.collider(this.monchi, this.map.pisos3, rotateCam);
     if (this.map.coin) this.physics.add.overlap(this.monchi, this.map.coin, coinCollected);
     if (this.map.portal) this.physics.add.overlap(this.monchi, this.map.portal, win);
     if (this.map.pisos4) this.physics.add.collider(this.monchi, this.map.pisos4, noFloat);
-    if (this.map.movingFloor) this.physics.add.collider(this.monchi, this.map.movingFloor, movingFloorsGrav);
+    // if (this.map.movingFloor) this.physics.add.collider(this.monchi, this.map.movingFloor, movingFloorsGrav);
+    if (this.map.movingFloor) this.physics.add.collider(this.monchi, this.map.movingFloor, rotateCam);
     if (this.map.movingFloorRot) this.physics.add.collider(this.monchi, this.map.movingFloorRot, movingFloorsGravRot);
+
 
     this.physics.world.on('worldbounds', (body: Phaser.Physics.Arcade.Sprite, top: boolean, down: boolean, left: boolean, right: boolean) => {
       if (down || top) lose()
@@ -135,11 +134,11 @@ class Scene1 extends Phaser.Scene {
     /* Attach controls to player */
     if (this.monchi && this.normalito) {
       this.monchi.checkMove(this.cursors)
-      if(this.map) this.map.animateBackground(this.monchi)
+      if (this.map) this.map.animateBackground(this.monchi)
     }
-    else if (this.monchi && this.normalito == false){
+    else if (this.monchi && this.normalito == false) {
       this.monchi?.checkMoveRot(this.cursors)
-      if(this.map) this.map.animateBackground(this.monchi)
+      if (this.map) this.map.animateBackground(this.monchi)
     }
   }
 }

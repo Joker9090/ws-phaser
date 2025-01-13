ebc8bb778daf347c396e2489aad860e4e07b2213
J <joaquincerruti54@gmail.com>
2025-01-13 13:30:59 -0300

    pa nano

--- game/containersMenu/containerAlbum.ts
+++ game/containersMenu/containerAlbum.ts
@@ -13,13 +13,12 @@ class containerAlbum extends Phaser.GameObjects.Container {
     backButton: Phaser.GameObjects.Image;
     banner:Phaser.GameObjects.Image;
     title: Phaser.GameObjects.Text;
-    masterManager: MasterManager
+    masterManager: MasterManager;
+    figuritas: Figuritas[]= [];
 
 
     constructor(scene: Phaser.Scene, config: ContainerMenuConfigType) {
-        super(scene, config.x, config.y)
-
- 
+        super(scene, config.x, config.y) 
         let masterManagerScene = scene.game.scene.getScene("MasterManager") as MasterManager;
         if (!masterManagerScene) {
             this.masterManager = new MasterManager();
@@ -39,7 +38,7 @@ class containerAlbum extends Phaser.GameObjects.Container {
             wordWrap: {
                 width: this.width * 0.9,
             },
-        }).setFontSize('60px').setVisible(false)
+        })
         this.backButton = scene.add.image(0, 0, "playBackButton")
         this.backButton.setPosition(this.backButton.width, this.height - this.backButton.height)
         this.backButton.setInteractive().on('pointerdown', () => {
@@ -63,20 +62,83 @@ class containerAlbum extends Phaser.GameObjects.Container {
 
 
         const arr: Phaser.GameObjects.GameObject[] = [
+            this.title,
             this.background,
             this.backButton, 
             this.banner,
-            this.title
+            ...this.figuritas
         ]
-        let posX = this.width/4.5; 
-        const spacing = 800;
-      
-        this.masterManager.imagenesAlbum.forEach(data => {
-            const figurita = new Figuritas(scene, posX, this.height / 1.7, data).setScale(0.8).setAlpha(0.8);
-            arr.push(figurita);
-            posX += spacing;
-        });
 
+        const spacing = 800;
+        let start =0;
+        let end = 2;
+        let step = 2
+
+        const updateFiguritas = () => {
+            // Limpiar las figuritas actuales
+            this.figuritas.forEach(figurita => figurita.destroy());
+            this.figuritas = [];
+            let posX = this.width / 3.5;
+            const showFiguritas = this.masterManager.imagenesAlbum.slice(start, end);
+            showFiguritas.forEach(data => {
+                const figurita = new Figuritas(scene, posX, this.height / 1.7, data).setScale(0.8).setAlpha(0.8);
+                this.figuritas.push(figurita);
+                posX += spacing;
+            });       
+            //@ts-ignore
+            arr.push([...this.figuritas]);
+
+        };
+        updateFiguritas();
+        const nextButton = this.scene.add.image(this.width - 80, this.height / 1.7, "backButton").setInteractive();
+        nextButton.on("pointerup", () => {
+            if (end < this.masterManager.imagenesAlbum.length) {
+                start += step;
+                end += step;
+                updateFiguritas();
+            }
+        });
+        nextButton.setInteractive().on('pointerdown', () => {
+            nextButton.setTexture('backButtonPressed')
+        })
+        nextButton.on('pointerup', () => {
+            nextButton.setTexture('backButton')
+            this.scene.sound.play('buttonSound')
+        })
+        nextButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER, () => {
+            nextButton.setTexture('backButtonHover')
+        })
+        nextButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT, () => {
+            nextButton.setTexture('backButton')
+        })
+    
+
+
+        const prevButton = this.scene.add.image(100, this.height / 1.7, "playBackButton").setInteractive();
+        prevButton.on("pointerup", () => {
+            if (start > 0) {
+                start -= step;
+                end -= step;
+                updateFiguritas();
+            }
+        });
+        prevButton.setInteractive().on('pointerdown', () => {
+            prevButton.setTexture('playBackButtonPressed')
+        })
+        prevButton.on('pointerup', () => {
+            prevButton.setTexture('playBackButton')
+            this.scene.sound.play('buttonSound')
+        })
+        prevButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER, () => {
+            prevButton.setTexture('playBackButtonHover')
+        })
+        prevButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT, () => {
+            prevButton.setTexture('playBackButton')
+        })
+    
+        arr.push(nextButton, prevButton)
+
+        console.log(arr)
         this.add(arr)
         scene.add.existing(this)
     }

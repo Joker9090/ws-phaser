bf6770399a093124f2bbba90730d4b2fbe5982b3
Barto <juanmartin.cerruti@gmail.com>
2025-02-21 14:38:30 -0300

    fix build

--- game/LoaderScene.ts
+++ game/LoaderScene.ts
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
-import AssetsLoader, { loadAssets, LoadTypes, SceneKeys } from "./multiScenes/assetsLoader";
+import { loadAssets, LoadTypes, SceneKeys } from "./multiScenes/assetsLoader";
 
 export default class LoaderScene extends Phaser.Scene {
  
@@ -9,25 +9,57 @@ export default class LoaderScene extends Phaser.Scene {
   progressBox: Phaser.GameObjects.Graphics | undefined
   monchi: Phaser.GameObjects.Sprite | undefined
   loadKey: SceneKeys[] = ["BaseLoad","Cinemato0", "Cinemato1", "Cinemato2", "Cinemato3","GamePlay1", "GamePlay2", "GamePlay3"]
-
+  alreadyLoaded: boolean = false
   constructor() {
     super({ key: "LoaderScene" });
   }
 
 
   preload() {
+
+    
+
     this.load.image("gameTitle", "/menu/initial/gameTitle.png");
     this.load.image("fondoCarga", "/menu/initial/fondoCarga.png");
     
     this.load.spritesheet("player",  "/game/player/playerSpriteSheet.png",  { frameWidth: 200, frameHeight: 200 });
-    this.runPreload(() => {
-      this.scene.start("MultiScene");
-    })
+    
+    // check if gameTitle, fondoCarga, player are loaded and callback the runPreload FN
+    
+    const scenesTitles: Array<SceneKeys> = this.loadKey
+    for (let i = 0; i < scenesTitles.length; i++) {
+      loadAssets[scenesTitles[i]].assets.map((sceneAssetConfig) => {
+        const type = sceneAssetConfig[0] as LoadTypes;
+        const name = sceneAssetConfig[1] as string;
+        const src = sceneAssetConfig[2] as string;
+        const config = sceneAssetConfig[3] as any;
+        if (config) {
+          this.load[type](name, src, config);
+        } else {
+          this.load[type](name, src);
+        }
+      });
+    }
+   
 
+    this.checkAssetLoaded()
+   
+  }
+
+  checkAssetLoaded() {
+    this.time.delayedCall(1000, () => {
+      console.log("CHECKING")
+     if (this.textures.exists("gameTitle") && this.textures.exists("fondoCarga") && this.textures.exists("player")) {
+        this.runPreload(() => {
+          this.scene.start("MultiScene");
+        })
+      } else {
+        this.checkAssetLoaded()
+      }
+    }, [], this)
   }
 
    runPreload(callback?: Function) {
-  
       if (!this.finished) {
         var width = this.cameras.main.width;
         var height = this.cameras.main.height;
@@ -102,20 +134,7 @@ export default class LoaderScene extends Phaser.Scene {
         });
   
   
-        const scenesTitles: Array<SceneKeys> = this.loadKey
-        for (let i = 0; i < scenesTitles.length; i++) {
-          loadAssets[scenesTitles[i]].assets.map((sceneAssetConfig) => {
-            const type = sceneAssetConfig[0] as LoadTypes;
-            const name = sceneAssetConfig[1] as string;
-            const src = sceneAssetConfig[2] as string;
-            const config = sceneAssetConfig[3] as any;
-            if (config) {
-              this.load[type](name, src, config);
-            } else {
-              this.load[type](name, src);
-            }
-          });
-        }
+        
         const ArcadeFont = this.add.text(0, 0, " .", {
           fontFamily: "Arcade",
         });

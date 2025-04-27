8e53d870c8605442ec2c5b403fcc3993d6112d3a
Barto <juanmartin.cerruti@gmail.com>
2025-04-27 12:20:14 -0300

    try fix scene change

--- game/MultiScene.ts
+++ game/MultiScene.ts
@@ -54,7 +54,7 @@ export default class MultiScene extends Phaser.Scene {
 
           //  this.makeTransition("MenuScene", undefined);
           //  this.makeTransition("TestScene", undefined);
-      }CAMBIANDO ESCENA
+      }
     });
 
   }
